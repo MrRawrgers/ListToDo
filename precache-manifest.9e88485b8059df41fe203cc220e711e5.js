8adf8cb5e517b1e1f6b6c9bd1eefeeffdae298a2
MrRawrgers <niall.w.rogers@gmail.com>
2020-07-28 10:55:07 +0100

    Updates

---
+++ precache-manifest.9e88485b8059df41fe203cc220e711e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a353c0595b0118ac99efb7d60f7e88",
+    "url": "/ListToDo/index.html"
+  },
+  {
+    "revision": "a1b412e759b2e0ecc277",
+    "url": "/ListToDo/static/css/main.1c7ed16f.chunk.css"
+  },
+  {
+    "revision": "34cca93dbc47bda87720",
+    "url": "/ListToDo/static/js/2.bc11d25d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ListToDo/static/js/2.bc11d25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b412e759b2e0ecc277",
+    "url": "/ListToDo/static/js/main.75ff059f.chunk.js"
+  },
+  {
+    "revision": "c875a0e7cdca82661350",
+    "url": "/ListToDo/static/js/runtime-main.025856c4.js"
+  }
+]);
